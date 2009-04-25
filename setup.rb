f74da6554d39b0258b1caade65794d003b3fa41f
vikhyat <vikhyatk@gmail.com>
2009-04-25 18:23:28 +0530

    Complete revisit, mostly.

--- setup.rb
+++ setup.rb
@@ -1,22 +1,24 @@
 require 'soundex'
-require 'pp'
 
-def log(arr)
+def save(hash)
   File.open('soundex_dictionary.dump', 'w+') do |line|
-    Marshal.dump(arr, line)
+    Marshal.dump(hash, line)
   end
 end
 
 def parse_dict(dict_path)
   arr = {}
   IO.readlines(dict_path).map do |line|
-    line.chomp!.force_encoding("ISO-8859-1")
-    line = line.split('/')[0]
-    arr[soundex(line)] << line rescue arr[soundex(line)] = [line]
+    begin
+      line = line.chomp.split('/')[0]
+      arr[soundex(line)] << line rescue arr[soundex(line)] = [line]
+    rescue  # Due to some encoding problems
+    end
   end
   arr
 end
 
 print "Working... "
-log(parse_dict('en_US.dic'))	# Replace en_US.dic with something else
+save(parse_dict('en_US.dic'))	# Replace en_US.dic with something else
 puts "Done!"
+
