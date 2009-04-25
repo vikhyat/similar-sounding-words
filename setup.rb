12a4e35bebe5350a52353b5b3507b2a49b14af6c
vikhyat <vikhyatk@gmail.com>
2009-04-25 17:12:49 +0530

    Custom soundex method and major performance enhancements.

--- setup.rb
+++ setup.rb
@@ -1,24 +1,36 @@
-require 'text'
+# coding: ascii
+
+# Modified version of http://snippets.dzone.com/posts/show/4530
+def soundex(string)
+  chars     = 'BPFVCSKGJQXZDTLMNR'
+  nums      = '111122222222334556'
+  chars_ex  = '^BPFVCSKGJQXZDTLMNR'
+  chars_del = '^A-Z'
+
+  str = string.upcase.delete(chars).squeeze
+
+  str[0..0] + str[1..-1]
+  .delete(chars_ex)
+  .tr(chars, nums)[0..2]
+  .ljust(3, '0') rescue ''
+end
 
 def log(arr)
-	File.open('soundex_dictionary.dump', 'w+') do |line|
-		Marshal.dump(arr, line)
-	end
+  File.open('soundex_dictionary.dump', 'w+') do |line|
+    Marshal.dump(arr, line)
+  end
 end
 
 def parse_dict(dict_path)
-	arr = {}
-	words = IO.readlines(dict_path).map do |line|
-		line.chomp!
-		line = line.split('/')[0]
-		if(arr.include? Text::Soundex.soundex(line))
-			arr[Text::Soundex.soundex(line)] += [line]
-		else
-			arr[Text::Soundex.soundex(line)] = [line]
-		end
-		puts "Added #{line}"
-	end
-	arr
+  arr = {}
+  IO.readlines(dict_path).map do |line|
+    line.chomp!.force_encoding("ISO-8859-1")
+    line = line.split('/')[0]
+    arr[soundex(line)] << line rescue arr[soundex(line)] = [line]
+  end
+  arr
 end
 
+print "Working... "
 log(parse_dict('en_US.dic'))	# Replace en_US.dic with something else if that's not the dictionary you are going to use
+puts "Done!"
